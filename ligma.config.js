01bf4f4ec4dfdcd5e554dc72e3a122c00f9b8a95
Nicky Marino <nicky@nickymarino.com>
2023-07-23 15:15:08 -0500

    Add solutions

--- ligma.config.js
+++ ligma.config.js
@@ -13,6 +13,7 @@ module.exports = {
         "ArrayList",
         "MazeSolver",
         "QuickSort",
+        "MergeSort",
         "BTPreOrder",
         "BTInOrder",
         "BTPostOrder",
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,43 @@
+
+function qs(arr: number[], low: number, high: number): void {
+    if (low >= high) {
+        return;
+    }
+
+    const pivotIdx = partition(arr, low, high);
+
+    qs(arr, low, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, high);
+}
+
+function partition(arr: number[], low: number, high: number): number { 
+    // The pivot selction doesn't need to be fancy; just use the last item
+    const pivot = arr[high];
+
+    let idx = low - 1;
+
+    // Weak sort
+    for (let i = low; i < high; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+
+    // Move the pivot to the right place in the sorted list
+    idx++;
+    arr[high] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+
+
 export default function quick_sort(arr: number[]): void {
+    // low and high are inclusive in quicksort, unlike most other algos
+    qs(arr, 0, arr.length - 1);
+
 
 }
